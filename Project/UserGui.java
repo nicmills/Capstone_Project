f344637bc71b22e8870592e53a9d2c789f26b36c
Nicolas Mills <millspec98@yahoo.com>
2015-04-24 12:19:12 -0500

    Messing with calling actions

--- Project/UserGui.java
+++ Project/UserGui.java
@@ -9,10 +9,11 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.io.*;
+import java.awt.Color;
 
 public class UserGui
 {
-    JButton connect, sendMsg, encryptMsg, decryptReceivedMsg;
+    JButton initializeServer, connectClient, sendMsg, encryptMsg, decryptReceivedMsg;
     JTextArea enterHostName, enterClientPort, enterServerPort, k1, k2, k3, plainMsg, encryptedMsg, rawMsg, transMsg;
     Client c1;
     Server s1;
@@ -31,7 +32,7 @@ public class UserGui
         //Close Program when the panel closes
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Private Messaging");
-        frame.setSize( 650, 650 );
+        frame.setSize( 650, 725 );
 
         //Center the Window
         frame.setLocationRelativeTo(null);
@@ -39,49 +40,56 @@ public class UserGui
         //Initialize Panel Object
         JPanel panel = new JPanel();
         panel.setLayout(null);
+        panel.setBackground( Color.BLACK );
 
         //add panel to frame
         frame.add(panel);
-        
-        //Buttons deal with sending and receiving Messages
-        connect = new JButton( "Click To Attempt Connection");
-        connect.setBounds( 50, 50, 200, 50);
-        panel.add( connect );
 
+        //Buttons to Initialize Servers and Connect Corresponding Clients
+        initializeServer = new JButton( "Click to Initialize Server" );
+        initializeServer .setBounds( 50, 50, 200, 50 );
+        panel.add( initializeServer );
+
+        connectClient = new JButton( "Click To Connect Client");
+        connectClient.setBounds( 50, 125, 200, 50);
+        panel.add( connectClient );
+
+        //Buttons deal with sending and receiving Messages
         sendMsg = new JButton("Send Message");
-        sendMsg.setBounds( 50, 250, 200, 100);
+        sendMsg.setBounds( 50, 400, 200, 50);
         panel.add( sendMsg );
 
         encryptMsg = new JButton("Encrypt Message");
-        encryptMsg.setBounds(50, 375, 200, 100);
+        encryptMsg.setBounds(50, 500, 200, 50);
         panel.add( encryptMsg );
 
         decryptReceivedMsg = new JButton("Decrypt Received Message");
-        decryptReceivedMsg.setBounds(50, 500, 200, 100);
+        decryptReceivedMsg.setBounds(50, 600, 200, 50);
         panel.add( decryptReceivedMsg );
-        
+
         //Initialize and add the ClickListener object to the buttons
         listener = new ClickListener( this );
-        connect.addActionListener( listener );
+        initializeServer.addActionListener( listener );
+        connectClient.addActionListener( listener );
         sendMsg.addActionListener( listener );
         encryptMsg.addActionListener( listener );
         decryptReceivedMsg.addActionListener( listener );
 
-        //Fields to enter target IP
+        //Fields to enter information about other computer
         enterHostName = new JTextArea( "Enter Host Name" );
-        enterHostName.setBounds(50, 100, 200, 25);
+        enterHostName.setBounds(50, 225, 200, 25);
         enterHostName.setVisible(true);
         enterHostName.setLineWrap(true);
         panel.add( enterHostName );
 
         enterClientPort = new JTextArea( "Enter Client Port" );
-        enterClientPort.setBounds(50, 150, 200, 25);
+        enterClientPort.setBounds(50, 275, 200, 25);
         enterClientPort.setVisible(true);
         enterClientPort.setLineWrap(true);
         panel.add( enterClientPort );
 
         enterServerPort = new JTextArea( "Enter Server Port" );
-        enterServerPort.setBounds(50, 200, 200, 25);
+        enterServerPort.setBounds(50, 325, 200, 25);
         enterServerPort.setVisible(true);
         enterServerPort.setLineWrap(true);
         panel.add( enterServerPort );
@@ -113,20 +121,20 @@ public class UserGui
         panel.add( plainMsg );
 
         encryptedMsg = new JTextArea( "Your Encrypted Message" );
-        encryptedMsg.setBounds( 300, 325, 150, 225 );
+        encryptedMsg.setBounds( 300, 350, 150, 225 );
         encryptedMsg.setVisible(true);
         encryptedMsg.setLineWrap(true);
         panel.add( encryptedMsg );
 
         //Fields to display the received messages
         rawMsg = new JTextArea( "Raw Message" );
-        rawMsg.setBounds( 450, 100, 150, 225);
+        rawMsg.setBounds( 475, 100, 150, 225);
         rawMsg.setVisible(true);
         rawMsg.setLineWrap(true);
         panel.add( rawMsg );
 
         transMsg = new JTextArea( "Translated Message" );
-        transMsg.setBounds( 450, 325, 150, 225);
+        transMsg.setBounds( 475, 350, 150, 225);
         transMsg.setVisible(true);
         transMsg.setLineWrap(true);
         panel.add( transMsg );
@@ -148,35 +156,35 @@ public class UserGui
 
         public void actionPerformed( ActionEvent event )
         {
-            if( event.getSource() == connect )
+            if( event.getSource() == initializeServer )
             {
-                int clientPort = Integer.parseInt( enterClientPort.getText() );
-                int serverPort = Integer.parseInt( enterServerPort.getText() );
                 try
                 {
+                    int clientPort = Integer.parseInt( enterClientPort.getText() );
                     s1 = new Server( clientPort, g1 );
-                    c1 = new Client( enterHostName.getText(), serverPort  );
-                }catch( IOException e )
+                }catch(IOException e )
                 {
                     e.printStackTrace();
                 }
-
-                while(true)
-                {
-                    if( event.getSource() == sendMsg )
-                    {
-                        c1.sendMsg( plainMsg.getText() );
-                    }
-                    else if( event.getSource() == encryptMsg )
-                    {
-                        //encrypter.scramble( plainMsg.getText() );
-                    }
-                    else if( event.getSource() == decryptReceivedMsg )
-                    {
-                        //encrypter.unScramble( rawMsg.getText() );
-                    }
-                }
+            }
+            if( event.getSource() == connectClient )
+            {
+                int serverPort = Integer.parseInt( enterServerPort.getText() );
+                c1 = new Client( enterHostName.getText(), serverPort  );
+            }
+            if( event.getSource() == sendMsg )
+            {
+                c1.sendMsg( plainMsg.getText() );
+            }
+            if( event.getSource() == encryptMsg )
+            {
+                //encrypter.scramble( plainMsg.getText() );
+            }
+            if( event.getSource() == decryptReceivedMsg )
+            {
+                //encrypter.unScramble( rawMsg.getText() );
             }
         }
     }
 }
+
